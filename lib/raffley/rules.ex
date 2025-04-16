1f5c84be023d44263e3957ecc66e2f79c43d0e72
~venir <luca.venir@wde.it>
2025-04-16 09:16:30 +0200

    a controller and some template

---
+++ lib/raffley/rules.ex
@@ -0,0 +1,18 @@
+defmodule Raffley.Rules do
+  def list_rules() do
+    [
+      %{
+        id: 1,
+        text: "Participants must have a high tolerance for puns and dad jokes 🤪"
+      },
+      %{
+        id: 2,
+        text: "Winner must do a victory dance when claiming their prize 🕺🏽"
+      },
+      %{
+        id: 3,
+        text: "Have fun! 🎟️"
+      }
+    ]
+  end
+end
