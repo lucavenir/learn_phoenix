b7ca6a218a4c69c6f447406ce630984ac8e0b007
~venir <luca.venir@wde.it>
2025-04-16 18:52:35 +0200

    updating live efforts

--- lib/heads_up_web/live/effort_live.ex
+++ lib/heads_up_web/live/effort_live.ex
@@ -6,4 +6,11 @@ defmodule HeadsUpWeb.EffortLive do
     socket = assign(socket, responders: 0, minutes_per_responder: 10)
     {:ok, socket}
   end
+
+  @impl true
+  def handle_event("add", %{"amount" => amount}, socket) do
+    amount = String.to_integer(amount)
+    socket = update(socket, :responders, fn responders -> responders + amount end)
+    {:noreply, socket}
+  end
 end
