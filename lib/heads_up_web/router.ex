774af17142458ec053ca7c7523138f63764d6b77
~venir <luca.venir@wde.it>
2025-05-03 15:56:07 +0200

    changed home page

--- lib/heads_up_web/router.ex
+++ lib/heads_up_web/router.ex
@@ -24,11 +24,12 @@ defmodule HeadsUpWeb.Router do
   scope "/", HeadsUpWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    get "/welcome", PageController, :home
 
     get "/tips", TipController, :index
     get "/tips/:id", TipController, :show
 
+    live "/", IncidentLive.Index
     live "/effort", EffortLive
     live "/incidents", IncidentLive.Index
     live "/incidents/:id", IncidentLive.Show
